c4856de6d05e6205227959c1f91f047f2f814ffd
avdambeck <audovisual@dambeck.org>
2026-05-19 20:18:23 -0400

    cleric 1st level spells

--- magic/spells.js
+++ magic/spells.js
@@ -8,23 +8,55 @@
  */
 
 let spells = [
-    {"name":"Fire Ball",
-     "desc":"Boom!!" ,
-     "level": ["CL3"]},
+    {"name":"Foo",
+     "desc":"Bar",
+     "level": ["Bap"]},
+
+    {"name":"Foo",
+     "desc":"Bar",
+     "revName":"Zig",
+     "revDesc":"Zag",
+     "level": ["Bap"]},
 
     {"name":"Cure Wounds",
-     "desc":"So cure, so heal.",
-     "level": ["CL1"],
-     "revName":"Cause Wounds",
-     "revDesc":"die die die"},
-
-    {"name":"Light",
-     "desc":"Wow i can seeeeee" ,
+     "desc":"A touched creature heals 1d6+1.",
      "level": ["CL1"]},
 
-    {"name":"Abracadabra",
-     "desc":"939393939393" ,
-     "level": ["CL1"]}
+    {"name":"Detect Magic",
+     "desc":"Enchanted objects, areas, and creatures glow while in the same zone as the caster. If they spend one turn studying a magic object, they learn general details about one of it's affects.</p><p> Lasts until fatigued.",
+     "level": ["CL1"]},
+
+    {"name":"Light",
+     "desc":"This spell has three uses:</p><ol>\
+<li><strong>Conjuring Light:</strong> A object begins to glow.</li>\
+<li><strong>Blinding:</strong> A creature becomes blind.</li>\
+<li><strong>Cancel Darkness:</strong> cancel a darkness spell.</li>\
+</ol><p>Lasts until fatigued.",
+     "revName":"Darkness",
+     "revDesc":"Fills one zone with magical darkness, preventing both normal sight and infravision. Like light, it can alternitively be used to blind a creature or dispell a light.",
+     "level": ["CL1"]},
+
+    {"name":"Protection from Chaos",
+     "desc":"Wards the caster or a touched creature from chaotic creatures and magic. Grants -1 to saves and dodges.",
+     "revName":"Protection from Law",
+     "revDesc":"Provides the same ward against law.",
+     "level": ["CL1"]},
+
+
+    {"name":"Purify Food and Water",
+     "desc":"Makes spoiled, rotten, poisonous, or otherwise contaminated foor and water pure and suitable for eating. Affects 6 quarts of fluid, one ration, or 12 people's worth of unpreserved food.",
+     "level": ["CL1"]},
+
+
+    {"name":"Remove Fear",
+     "desc":"The touched creature is calmed and warded against fear. Magically induced fear is dispelled.</p><p>Lasts until fatigued",
+     "revName":"Cause Fear",
+     "revDesc":"A chosen creature becomes overwhelmed with fear and begins to flee. </p><p>Lasts until fatigued.",
+     "level": ["CL1"]},
+
+  {"name":"Null",
+     "desc":"" ,
+     "level": [""]}
 ]
 
 function spellDesc(spell) {
@@ -47,7 +79,7 @@ function getLevelList(code) {
 
 function spellListAll() {
 //    let list = sortSpellAlpha(spells);
-    let list = getLevelList("CL1");
+    let list = getLevelList("CL2");
     let len = list.length;
 
     let text = "";
