21598d2cfdebdce57d3e64d6241b1a389ec7ff89
avdambeck <audovisual@dambeck.org>
2026-08-01 09:41:43 -0400

    unique only

--- export/check-pdf.sh
+++ export/check-pdf.sh
@@ -1,1 +1,1 @@
-find . -name "*.html" | sort > temp-html_files.txt; sort export/pdf.txt > temp-paths.txt; comm -3 temp-html_files.txt temp-paths.txt; rm temp-html_files.txt temp-paths.txt
+find . -name "*.html" | sort > temp-html_files.txt; sort export/pdf.txt > temp-paths.txt; comm -3 temp-html_files.txt temp-paths.txt | sort -u; rm temp-html_files.txt temp-paths.txt
